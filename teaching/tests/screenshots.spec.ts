20674b79d723c112dc53be1aa79e35323de1ad78
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-08-30 14:41:24 +0100

    adds screenshot stuff

---
+++ teaching/tests/screenshots.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+test.skip("Take a screenshot", async ({ page, browserName }) => {
+  await page.goto(
+    "https://www.gov.uk/government/organisations/companies-house"
+  );
+
+  await page.screenshot({
+    path: `screenshots/${new Date().getTime()}-${browserName}-desktop-screenshot.jpg`,
+    fullPage: true,
+  });
+
+  await page.setViewportSize({ width: 640, height: 480 });
+
+  await page.screenshot({
+    path: `screenshots/${new Date().getTime()}-${browserName}-mobile-screenshot.jpg`,
+    fullPage: true,
+  });
+});
+
+test("Visual regression", async ({ page }) => {
+  await page.goto(
+    "https://www.gov.uk/government/organisations/companies-house"
+  );
+
+  await expect(page).toHaveScreenshot({
+    maxDiffPixels: 120000,
+    fullPage: true,
+  });
+});
